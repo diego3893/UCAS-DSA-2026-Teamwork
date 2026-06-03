b25fd09e6f3a1f42c3a82df2db3d5b76acee5490
diego3893 <diegozcx@foxmail.com>
2026-06-03 20:29:14 +0800

    fix: 修复对于空树的处理，修复根节点处理

--- src/BalancedTree/src/display/display.cpp
+++ src/BalancedTree/src/display/display.cpp
@@ -1,12 +1,17 @@
 #include "display.h"
 #include "common.h"
 #include <stack>
+#include <cstdio>
 
 using namespace std;
 
 void showTree(){
+    if(rootIndex == 0){
+        printf("[当前为空树]\n");
+        return;
+    }
     stack<TreeNode> s;
-    s.push(tree[0]);
+    s.push(tree[rootIndex]);
     while(!s.empty()){
         TreeNode cur = s.top();
         s.pop();
