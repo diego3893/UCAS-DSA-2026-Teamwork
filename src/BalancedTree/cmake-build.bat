c52848d8dc8b92818613f177bf61405d1491815b
diego3893 <diegozcx@foxmail.com>
2026-06-03 21:00:30 +0800

    feat: 编译脚本

---
+++ src/BalancedTree/cmake-build.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set "CMAKE_GENERATOR=MinGW Makefiles"
+set "PROJECT_NAME=BalancedTree"
+set "MAKE_CMD=mingw32-make"
+
+if exist "build" (
+    rd /s /q "build"
+)
+
+mkdir build
+cd build
+
+cmake -G "%CMAKE_GENERATOR%" ..
+
+%MAKE_CMD%
+
+echo Run %PROJECT_NAME%.exe now?(Y/N)
+set /p RUN_CHOICE=
+if /i "%RUN_CHOICE%"=="Y" (
+    start %PROJECT_NAME%.exe
+)
+
+exit /b 0
